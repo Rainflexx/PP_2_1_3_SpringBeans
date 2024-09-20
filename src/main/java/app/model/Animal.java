2f06f36e11ea65b5a2e48997a2304fac5d8716ba
Rainflexx <natsakov00@gmail.com>
2024-09-20 17:58:43 +0300

    Message

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,8 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public abstract class Animal {
 }
